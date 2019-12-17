df0d50ca37b2d1d729a591eefb39540a53dbcd47
Yuhei Okazaki <okazaki@fusic.co.jp>
2019-12-18 08:59:45 +0900

    add webpacker:compile to entry_development

--- script/entrypoint_development.sh
+++ script/entrypoint_development.sh
@@ -1,4 +1,5 @@
 if [ -e /app/tmp/pids/server.pid ]; then rm -f /app/tmp/pids/server.pid; fi
 bundle exec rails db:migrate
+bundle exec rails webpacker:compile
 bundle exec rails s -p 80 -b 0.0.0.0
 # bundle exec foreman start
