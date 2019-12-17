d78ac5f3593aac1134e2dc2afcdd1346dd51c81b
Yuhei Okazaki <okazaki@fusic.co.jp>
2019-12-18 07:03:37 +0900

    add config hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.hosts << "ecs-rails-app-232545850.ap-northeast-1.elb.amazonaws.com"
 end
